4a4dfffe52b861c6959d6164305f7d509127d1a0
jack <1395093509@qq.com>
2018-02-22 16:14:34 +0800

    更新

--- src/pages.js
+++ src/pages.js
@@ -1,14 +1,17 @@
+// 初始化项目
 import { me } from './origin';
-import { mapTo } from '../utils/map';
+import { mapTo } from './utils';
 
 let miniConfig = {};
 let pages;
 let tabBar;
 let tabBarList;
 
+// 此文件路径在 ./node_modules/x-mini/lib/pages.js
+// 所以引入文件需要 跳出四层
 switch (me.miniType) {
   case 'aliapp':
-    miniConfig = require('../app.json');
+    miniConfig = require('../../../../app.json');
     pages = miniConfig.pages;
     tabBar = miniConfig.tabBar;
     // { pages = [], tabBar = {} } = miniConfig;
@@ -51,3 +54,15 @@ console.log('所有注册页面：');
 console.log(regPages);
 
 export default regPages;
+// export default {
+//   init() {
+//     // 初始化项目
+
+//   },
+//   all() {
+//     return regPages;
+//   },
+//   tabs() {
+//     return tabPages;
+//   },
+// };
