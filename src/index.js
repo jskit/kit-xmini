136c5874ba51dd862952aa28b5e2440746f08b64
jack <1395093509@qq.com>
2018-02-23 10:11:12 +0800

    更新pages

--- src/index.js
+++ src/index.js
@@ -4,20 +4,9 @@ import * as utils from './utils';
 import pages from './pages';
 import store from './store';
 
-const options = {
-  configPath: './src',
-};
-
 export default {
-  create(opts) {
-    opts = { ...options, ...opts };
-    pages.init(opts.configPath);
-
-    return {
-      ...utils,
-      ...mini,
-      pages,
-      store,
-    }
-  },
+  ...utils,
+  ...mini,
+  pages,
+  store,
 }
--- src/pages.js
+++ src/pages.js
@@ -3,41 +3,36 @@ import { me } from './origin';
 import { mapTo } from './utils';
 
 let miniConfig = {};
-let pages;
+let pages = [];
 let tabBar;
 let tabBarList;
-let tabPages;
+let tabPages = [];
 
 // 此文件路径在 ./node_modules/x-mini/lib/pages.js
 // 所以引入文件需要 跳出四层，而且引入文件的配置文件app.json 的位置并不固定，
 // 就需要使用时，传入配置路径
 // 所以推荐使用全局变量来处理
-switch (me.miniType) {
-  case 'aliapp': {
-    // 使用一个全局变量来替代 $global
-    // miniConfig = require('../../../../app.json');
-    const { pagesConfig = {}, tabsConfig = {} } = $global;
-    pages = Object.keys(pagesConfig);
-    tabPages = Object.keys(tabsConfig);
-    // tabBarList = miniConfig.tabBar;
-    // { pages = [], tabBar = {} } = miniConfig;
-    // tabBarList = tabBar.list || [];
-    break;
-  }
-  case 'wxapp':
-    // __wxConfig 微信小程序内的一个全局变量
-    miniConfig = __wxConfig;
-    /* eslint prefer-destructuring: 0 */
-    pages = miniConfig.pages;
-    tabBar = miniConfig.tabBar;
-    // { pages = [], tabBar = {} } = miniConfig;
-    tabBarList = tabBar.list;
-    tabPages = mapTo(tabBarList, (item) => {
-      return item.pagePath.replace('.html', '')
-    });
-    break;
-  default:
-    // do nothing
+if (me.miniType === 'alipay') {
+  // 使用一个全局变量来替代 $global
+  // miniConfig = require('../../../../app.json');
+  const { pagesConfig = {}, tabsConfig = {} } = $global;
+  pages = Object.keys(pagesConfig);
+  tabPages = Object.keys(tabsConfig);
+  // tabBarList = miniConfig.tabBar;
+  // { pages = [], tabBar = {} } = miniConfig;
+  // tabBarList = tabBar.list || [];
+}
+if (me.miniType === 'wxapp') {
+  // __wxConfig 微信小程序内的一个全局变量
+  miniConfig = __wxConfig;
+  /* eslint prefer-destructuring: 0 */
+  pages = miniConfig.pages;
+  tabBar = miniConfig.tabBar;
+  // { pages = [], tabBar = {} } = miniConfig;
+  tabBarList = tabBar.list;
+  tabPages = mapTo(tabBarList, (item) => {
+    return item.pagePath.replace('.html', '')
+  });
 }
 
 console.log('test');
@@ -54,7 +49,11 @@ function pagesMap(pageArr) {
 
 const regPages = pagesMap(pages);
 
-[regPages.defaultPage] = pages[0].split('/').reverse();
+if (pages.length) {
+  [regPages.defaultPage] = pages[0].split('/').reverse();
+} else {
+  console.error(`pages 为空，发生错误`);
+}
 
 // 支付宝变量取配置时，没拿到数组排序
 // 所以如果 index 存在，则设置index为默认首页
@@ -63,6 +62,10 @@ if (me.miniType === 'aliapp') {
 }
 
 regPages.tabPages = pagesMap(tabPages);
+
+regPages.updateDefaultPage = (pageName) => {
+  regPages.defaultPage = regPages[pageName];
+}
 
 console.log('所有注册页面：');
 console.log(regPages);
