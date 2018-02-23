e6dc2c51ff1e6ffab4733a399fd84b673773dcc6
jack <1395093509@qq.com>
2018-02-23 11:04:36 +0800

    更新文档及使用方法

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # x-mini
 
-封装小程序
+封装小程序，针对不同的小程序，不要将判断封装在里面，请自己选择使用
+
+```js
+// 需要注意的文件
+./mini/rewrite/index.js
+./mini/pages.js
+```
+
+## 使用
+
+请把代码下载到项目根目录 `./mini` 中，统一通过 `./utils/mini.js` 暴露接口来引用
+
+```js
+// ./utils/mini.js
+import mini from '../mini'
+
+export default mini;
+```
 
 ## Testing
 
--- src/rewrite/index.js
+++ src/rewrite/index.js
@@ -1,18 +1,12 @@
 // 针对小程序挂载的方法进行覆写或扩展，优化增强调用方法
 
 import {
-  me,
+  // me,
   xPage,
 } from '../origin';
 
-let rewriteMe;
-
-if (me.miniType === 'aliapp') {
-  rewriteMe = require('./aliapp');
-}
-if (me.miniType === 'wxapp') {
-  rewriteMe = require('./wxapp');
-}
+const rewriteMe = require('./aliapp');
+// const rewriteMe = require('./wxapp');
 
 export default {
   me: rewriteMe,
