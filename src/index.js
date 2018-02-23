794e6bbc5bc9e79ba8acc56231e1821fb67e430f
jack <1395093509@qq.com>
2018-02-23 10:47:40 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 // 扩展
-import mini from './mini';
-import * as utils from './utils';
-import pages from './pages';
-import store from './store';
+import XMini from './mini';
+import origin from './origin';
 
-export default {
-  ...utils,
-  ...mini,
-  pages,
-  store,
-}
+XMini.origin = origin;
+
+export default XMini;
--- src/mini.js
+++ src/mini.js
@@ -2,23 +2,53 @@
 // 扩展
 import origin from './origin';
 import mixins from './mixins/index';
+import { noop } from './utils';
 
-export default {
-  ...origin,
-  me: {
-    ...mixins.me,
-    ...origin.me,
-  },
-  xApp(opts = {}) {
-    return origin.xApp({
-      ...mixins.app,
+let mini;
+const defaultOptions = {
+  me: {},
+  xApp: noop,
+  xPage: noop,
+  getCurrentPages: noop,
+}
+
+class XMini {
+  constructor(opts) {
+    if (mini) {
+      console('XMini() can only be called once');
+      return mini;
+    }
+    this.config = {
+      ...defaultOptions,
       ...opts,
+    };
+    origin.config(this.config);
+    this.init();
+  }
+
+  init() {
+    Object.assign(this, {
+      ...origin,
+      me: {
+        ...mixins.me,
+        ...origin.me,
+      },
+      xApp(opts = {}) {
+        return origin.xApp({
+          ...mixins.app,
+          ...opts,
+        });
+      },
+      xPage(opts = {}) {
+        return origin.xPage({
+          ...mixins.page,
+          ...opts,
+        });
+      },
     });
-  },
-  xPage(opts = {}) {
-    return origin.xPage({
-      ...mixins.page,
-      ...opts,
-    });
-  },
-};
+    mini = this;
+  }
+}
+
+export default XMini;
+
