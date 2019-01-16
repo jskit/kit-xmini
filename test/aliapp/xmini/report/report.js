428fe6b682520c7537043871cd17b0b923cbecbc
jack <1395093509@qq.com>
2019-01-16 16:30:35 +0800

    同步 aliapp

--- test/aliapp/xmini/report/report.js
+++ test/aliapp/xmini/report/report.js
@@ -3,6 +3,8 @@ import {
 } from '../mini/utils';
 import native from '../mini/native';
 
+const miniappType = typeof my !== 'undefined' ? 'aliapp' : 'wxapp';
+
 function report(err) {
   // https://tongji.doweidu.com/log.php
   const {
@@ -19,14 +21,26 @@ function report(err) {
   const request = host === 'aliapp' ? 'httpRequest' : 'request';
   const pageInfo = me.$getPageInfo();
   const systemInfo = me.$getSystemInfo() || {};
+  const os = systemInfo['platform'] === 'iPhone OS' ? 'iOS' : systemInfo['platform'];
+
   me[request]({
     url: 'https://tongji.doweidu.com/log.php',
     method: 'POST',
     data: {
-      ...systemInfo,
+      miniappType,
+      os, // 客户端平台 Android iOS
+      osVersion: systemInfo['system'], // 操作系统版本
+      host: systemInfo['app'] || 'wechat', // 当前运行的客户端 alipay wechat
+      hostVersion: systemInfo['version'], // 宿主版本号
+      sdkVersion: systemInfo['SDKVersion'] || '-1', // 客户端基础库版本
+      language: systemInfo['language'], // 设置的语言
+      phoneBrand: systemInfo['brand'], // 手机品牌
+      phoneModel: systemInfo['model'], // 手机型号
+      pixelRatio: systemInfo['pixelRatio'], // 设备像素比
       pagePath: pageInfo.pagePath,
       pageQuery: JSON.stringify(pageInfo.pageQuery),
       appName: appName,
+      project: appName,
       value: err,
     },
     dataType: 'json',
